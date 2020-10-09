ca200b55e149a69c6aeb9b8af013504bdefc527e
Giselda Valedon Guerra <giselda.valedon@gmail.com>
2020-10-09 18:41:15 -0400

    Update webpack.dev.js
    mentor recommendations

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,6 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const WorkboxPlugin = require('workbox-webpack-plugin')
 
-
 module.exports = {
     entry: './src/client/index.js',
     output: {
@@ -41,5 +40,6 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         })
+        new WorkboxPlugin.GenerateSW()
     ]
 }
